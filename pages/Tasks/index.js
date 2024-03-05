c0eafe0891e025da4975bf949612e2101852a404
Sumit <jagtasumit668@gmail.com>
2024-03-05 23:30:18 +0530

    key

--- pages/Tasks/index.js
+++ pages/Tasks/index.js
@@ -1,7 +1,5 @@
 import Sidebar from "@/components/sidebar/Sidebar";
-import React, { useState } from "react";
 
 export default function index() {
-  const [open, setOpen] = useState(false);
   return <div className="App">hello</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -98,7 +98,7 @@ export default function Home() {
           >
             {Array.isArray(tasklist) &&
               tasklist.map((task) => (
-                <div className="m-5">
+                <div className="m-5" key={task.id}>
                   <div
                     class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
                     style={{
